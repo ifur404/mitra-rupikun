8d059ad3c2a360478a76e5060636dd10294f9e19
Ahmad Saepurramdan <ahmadsaepurramdan@gmail.com>
2025-01-23 12:23:30 +0700

    update

--- app/lib/ledger.server.ts
+++ app/lib/ledger.server.ts
@@ -1,11 +1,12 @@
-import { eq } from "drizzle-orm"
+import { asc, eq } from "drizzle-orm"
 import { db } from "~/drizzle/client.server"
 import { productTable } from "~/drizzle/schema"
 
 export async function getListDB(env: Env, category?:string){
     const mydb = db(env.DB)
     const result = await mydb.query.productTable.findMany({
-        where: eq(productTable.category, category || '').if(category)
+        where: eq(productTable.category, category || '').if(category),
+        orderBy: asc(productTable.name),
     })
     return result
 }
